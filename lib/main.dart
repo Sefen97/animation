5e752d42dd80422a0cc2bcdf9f2bf16b150f705c
sefen <sefenfsragshafik991@gmail.com>
2023-12-02 23:43:55 +0200

    add hero to navigation

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,8 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
     _controller =
         AnimationController(vsync: this, duration: const Duration(seconds: 2))
           ..repeat();
-    _animation = CurvedAnimation(parent: _controller, curve: Curves.linearToEaseOut);
+    _animation =
+        CurvedAnimation(parent: _controller, curve: Curves.linearToEaseOut);
     super.initState();
   }
 
@@ -32,10 +33,52 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
           title: const Text("Animation"),
         ),
         body: Center(
-          child: RotationTransition(
-            turns: _animation,
-            child: FlutterLogo(size: 150),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              // Center(
+              //   child: RotationTransition(
+              //     turns: _animation,
+              //     child: const FlutterLogo(size: 150),
+              //   ),
+              // ),
+              Builder(
+                  builder: (context) => InkWell(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (_) => const TestNavigateHero()));
+                      },
+                      child: const Hero(
+                          tag: 'To_Flutter', child: FlutterLogo(size: 50)))),
+            ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class TestNavigateHero extends StatefulWidget {
+  const TestNavigateHero({super.key});
+
+  @override
+  State<TestNavigateHero> createState() => _TestNavigateHeroState();
+}
+
+class _TestNavigateHeroState extends State<TestNavigateHero> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Hero(tag: "To_Flutter", child: FlutterLogo(size: 150)),
+            FadeInImage(placeholder: AssetImage(''), image: NetworkImage(''))
+          ],
         ),
       ),
     );
